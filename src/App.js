202b424c5113324ea06a78c32ac17e19059a1682
Anandoganiya <Anandoganiya@gmail.com>
2022-06-08 18:03:01 +0530

    added protected route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import {Candidates,LongIn,SignUp,AddCandidate,EditCandidate} from './pages'
+import ProtectedRoute from './helper/ProtectedRoute';
+import useAuth from './hooks/userAuth'
 function App() {
+  const {userAuth} = useAuth()
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Candidates/>}/>
+        <Route path='/' element={<ProtectedRoute user={userAuth}/>}>
+          <Route path='/' element={<Candidates/>}/>
+        </Route>
         <Route path='/add-candidate/:currentUserId' element={<AddCandidate/>}/>
         <Route path='/edit-candidate/:candDocId' element={<EditCandidate/>}/>
         <Route path='/login' element={<LongIn/>}/>
